6ebedcd7f8ab548774baadad2184b1c1d67732c9
Demian Rodriguez <demian85@gmail.com>
2018-06-28 14:52:24 -0300

    bot working

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
-module.exports = app => {
-  // Your code here
+module.exports = (app) => {
+
   app.log('Yay, the app was loaded!')
 
-  // For more information on building apps:
-  // https://probot.github.io/docs/
+  app.on('issues.opened', async (context) => {
+    // `context` extracts information from the event, which can be passed to
+    // GitHub API calls. This will return:
+    //   {owner: 'yourname', repo: 'yourrepo', number: 123, body: 'Hello World!}
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+    app.log('issue opened:', context.payload.issue);
+
+    const matches = context.payload.issue.body.match(/https?:\/\/[^\s]+/guis);
+    const urls = matches.map(v => `"${v}"`).join(' ');
+
+    console.log('matches:', matches, urls);
+    console.log('urls:', urls);
+
+    if (matches.length === 0) {
+      app.log('no links found in issue, skipping...')
+      return;
+    }
+    
+    app.log('searching for similar issues...');
+
+    const searchResult = await context.github.search.issues({
+      q: `type:issue in:body ${urls}`,
+      per_page: 100
+    });
+
+    if (!searchResult.data || searchResult.data.items.length === 0) {
+      app.log('no similar issues found, skipping...')
+      return;
+    }
+
+    app.log('found results:', searchResult.data.items);
+
+    const issues = searchResult.data.items.map(item => item.url);
+    const params = context.issue({
+      body: `Similar issues were found containing the suggested URLs:\n\n ${issues.join('\n')}\n\n.Please, check if content is not a duplicate.\n\nThis is an automated response.`
+    })
+
+    // Post a comment on the issue
+    return context.github.issues.createComment(params);
+  })
 }
